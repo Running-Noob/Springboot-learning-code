660cc33318cf70ef531575e75b1e42066073a9a8
Running-Noob <fang_zhuoyue@163.com>
2024-02-29 09:48:04 +0800

    add: springboot_07_config

---
+++ springboot_07_config/src/main/java/com/f/springboot/Springboot07ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.f.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot07ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot07ConfigApplication.class, args);
+    }
+
+}
---
+++ springboot_07_config/src/test/java/com/f/springboot/Springboot07ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
